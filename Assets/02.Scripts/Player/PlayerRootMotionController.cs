67eaecc4e6b33a9a5ea4442bc829c98e15707175
Leekiahn <kiahn2000@gmail.com>
2025-12-12 07:56:11 +0900

    feat: 애니메이션 클립 발소리 이벤트 구현

--- Assets/02.Scripts/Player/PlayerRootMotionController.cs
+++ Assets/02.Scripts/Player/PlayerRootMotionController.cs
@@ -5,18 +5,19 @@ public class PlayerRootMotionController : MonoBehaviour
     private PlayerInputHandler inputHandler;
     private Animator animator;
     private new Rigidbody rigidbody;
+    private AudioSource audioSource;
     private float smoothDampTime = 0.1f;
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private float groundDistance;
     [SerializeField] private Transform groundCheck;
+    [SerializeField] private AudioClip[] walkFootStepSound;
+    [SerializeField] private AudioClip[] SprintFootStepSound;
 
     private readonly int hashMoveX = Animator.StringToHash("MoveX");
     private readonly int hashMoveY = Animator.StringToHash("MoveY");
     private readonly int hashIsSprinting = Animator.StringToHash("IsSprinting");
     private readonly int hashDiveRoll = Animator.StringToHash("DiveRoll");
     private readonly int hashIsGrounded = Animator.StringToHash("IsGrounded");
-    private readonly int hashDiveRollX = Animator.StringToHash("DiveRollX");
-    private readonly int hashDiveRollY = Animator.StringToHash("DiveRollY");
 
     private bool hasDiveRolled = false;
 
@@ -25,6 +26,7 @@ public class PlayerRootMotionController : MonoBehaviour
         inputHandler = GetComponent<PlayerInputHandler>();
         animator = GetComponent<Animator>();
         rigidbody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Start()
@@ -62,5 +64,23 @@ public class PlayerRootMotionController : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+    }
+
+    private void OnWalkFootStep()
+    {
+        if (inputHandler.MoveInput.magnitude > 0.1f && IsGrounded())
+        {
+            int index = Random.Range(0, walkFootStepSound.Length);
+            audioSource.PlayOneShot(walkFootStepSound[index]);
+        }
+    }
+
+    private void OnSprintFootStep()
+    {
+        if (inputHandler.MoveInput.magnitude > 0.1f && IsGrounded())
+        {
+            int index = Random.Range(0, SprintFootStepSound.Length);
+            audioSource.PlayOneShot(SprintFootStepSound[index]);
+        }
     }
 }
